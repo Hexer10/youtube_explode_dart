62f4bf4eaee7d3b4d29f9beacade52888274da8f
Mattia <hexer504@pm.me>
2023-12-10 21:23:45 +0100

    Increate test timeout

--- test/playlist_test.dart
+++ test/playlist_test.dart
@@ -77,5 +77,5 @@ void main() {
         .getVideos('RDCLAK5uy_m9Rw_g5eCJtMhuRgP1eqU3H-XW7UL6uWQ')
         .length;
     expect(playlistVideosCount, greaterThan(100));
-  });
+  }, timeout: const Timeout(Duration(minutes: 2)));
 }
