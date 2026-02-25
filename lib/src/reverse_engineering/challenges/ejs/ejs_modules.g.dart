8c4f331d634f91a8840e1ae41ab43164a1587c25
Mattia <hexer504@pm.me>
2026-02-25 15:14:38 +0100

    update ejs module

--- lib/src/reverse_engineering/challenges/ejs/ejs_modules.g.dart
+++ lib/src/reverse_engineering/challenges/ejs/ejs_modules.g.dart
@@ -1,15 +1,15 @@
 // GENERATED FILE FROM 'bin/ejs_updater.dart' DO NOT EDIT MANUALLY
-// VERSION=0.3.1
+// VERSION=0.5.0
 
 const modules = {
   'lib': {
     'url':
-        'https://github.com/yt-dlp/ejs/releases/download/0.3.1/yt.solver.lib.min.js',
+        'https://github.com/yt-dlp/ejs/releases/download/0.5.0/yt.solver.lib.min.js',
     'hash': 'c55987fe697e5b9ee18830163f7af85327e9bb5c3e674b969d38c8d205eaa577'
   },
   'core': {
     'url':
-        'https://github.com/yt-dlp/ejs/releases/download/0.3.1/yt.solver.core.min.js',
-    'hash': '8d28ee906fd0acf07aa7a482135e38b6ee706392d46cd4ab0b51503d4cc18f18'
+        'https://github.com/yt-dlp/ejs/releases/download/0.5.0/yt.solver.core.min.js',
+    'hash': 'af673c4473f61b49f2e513e99ad7ce4b4e7d8ef4e861abc989e8b6a6e1d605aa'
   }
 };
